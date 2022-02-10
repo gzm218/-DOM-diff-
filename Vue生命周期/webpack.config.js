a8cec5409e2204997e455c61dc48cb526c428dea
阿勒锅锅 <57820381+gzm218@users.noreply.github.com>
2022-02-10 17:21:00 +0800

    Add files via upload

---
+++ Vue生命周期/webpack.config.js
@@ -0,0 +1,19 @@
+//https://webpack.js.org/
+const path = require('path');
+
+module.exports = {
+  //入口
+  entry: './src/index.js',
+  output: {
+    //虚拟打包路径,就是说文件不会真正的生成，而是在8080端口虚拟生成
+    publicPath: 'xuni',
+    //打包出的文件名
+    filename: 'bundle.js',
+  },
+  devServer: {
+    //端口号
+    port: 8080,
+    //静态资源文件夹
+    contentBase: 'www',
+  },
+};
