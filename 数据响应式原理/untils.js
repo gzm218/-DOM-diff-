15655d68023e9471390fb83f2977dea897fd00a2
阿勒锅锅 <57820381+gzm218@users.noreply.github.com>
2022-02-10 17:23:26 +0800

    Delete untils.js

--- 数据响应式原理/untils.js
+++
@@ -1,9 +0,0 @@
-//遍历工具函数 
-export const def = function (obj, key, value, enumerable) {
-  Object.defineProperty(obj, key, {
-    value,
-    enumerable,
-    writable: true,
-    configurable: true,
-  });
-};
